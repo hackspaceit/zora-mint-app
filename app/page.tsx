8a9d2032893f6b1d3601d78af10e9a8556ecd3c0
hackspaceit <iniakunbisnisku@gmail.com>
2025-05-20 00:07:06 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -97,8 +97,7 @@ export default function App() {
         </header>
 
         <main className="flex-1">
-          {activeTab === "home" && <Home setActiveTab={setActiveTab} />}
-          {activeTab === "features" && <Features setActiveTab={setActiveTab} />}
+          
          
         </main>
 
