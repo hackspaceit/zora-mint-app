34857c398d0bdad3756a45e39a1cb6d572b5bd35
hackspaceit <iniakunbisnisku@gmail.com>
2025-05-20 03:14:20 +0700

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import "@coinbase/onchainkit/styles.css";
 import type { Metadata, Viewport } from "next";
 import "./globals.css";
 import { Providers } from "./providers";
-import { config } from './config'
+
 
 export const viewport: Viewport = {
   width: "device-width",
