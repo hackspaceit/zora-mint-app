0fe310b11e6c34861c6793f59b643757684af9b5
hackspaceit <iniakunbisnisku@gmail.com>
2025-05-19 23:58:14 +0700

    update

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,13 +1,18 @@
 "use client";
 
 import { type ReactNode } from "react";
-import { base } from "wagmi/chains";
+import { celo } from "wagmi/chains";
 import { MiniKitProvider } from "@coinbase/onchainkit/minikit";
+
+
+
+
 export function Providers(props: { children: ReactNode }) {
   return (
     <MiniKitProvider
       apiKey={process.env.NEXT_PUBLIC_ONCHAINKIT_API_KEY}
-      chain={base}
+      projectId={process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID}
+      chain={celo}
       config={{
         appearance: {
           mode: "auto",
