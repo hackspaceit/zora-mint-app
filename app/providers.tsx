ca1e2807b64ce46d6f70632c04ae1caac69a78f4
hackspaceit <iniakunbisnisku@gmail.com>
2025-05-19 23:59:39 +0700

    update

--- app/providers.tsx
+++ app/providers.tsx
@@ -10,7 +10,7 @@ import { MiniKitProvider } from "@coinbase/onchainkit/minikit";
 export function Providers(props: { children: ReactNode }) {
   return (
     <MiniKitProvider
-      apiKey={process.env.NEXT_PUBLIC_ONCHAINKIT_API_KEY}
+      
       projectId={process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID}
       chain={celo}
       config={{
